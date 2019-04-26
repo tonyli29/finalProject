a4e96f035636253db0379a12988033104a87b53a
Tony Li <litony1999@gmail.com>
2019-04-26 18:13:23 -0400

    postqsql

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -91,8 +91,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bmspsycx',
+        'USER': 'bmspsycx',
+        'PASSWORD': 'H6eQQzbTn13WAXG3_EmZAoGnr_LkVppL',
+        'HOST': 'hansken.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 

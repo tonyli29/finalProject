f7aa8cd964ae42c5557b234dbe8bfaafcd5df7c2
Tony Li <litony1999@gmail.com>
2019-04-30 00:24:08 -0400

    added db_databvase

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -91,14 +91,15 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bmspsycx',
-        'USER': 'bmspsycx',
-        'PASSWORD': 'H6eQQzbTn13WAXG3_EmZAoGnr_LkVppL',
-        'HOST': 'hansken.db.elephantsql.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation

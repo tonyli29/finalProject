6962d960c35748905db35642789abb22e60537e8
Tony Li <litony1999@gmail.com>
2019-04-30 00:44:58 -0400

    asd

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -96,7 +98,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation

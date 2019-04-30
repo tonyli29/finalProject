e0486dd5a0c093b0d46dccb71a005b2282ccb602
Tony Li <litony1999@gmail.com>
2019-04-30 00:53:52 -0400

    deleted import gjango-heroku

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,9 +134,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 
 # Static files (CSS, JavaScript, Images)

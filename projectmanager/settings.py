afd66a2659f793fdad5efd11f4c80488cef03441
Tony Li <litony1999@gmail.com>
2019-05-08 17:04:34 -0400

    f

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -135,3 +136,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
